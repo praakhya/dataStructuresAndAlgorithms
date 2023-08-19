3c9827862246258623155012ae1196bbe74feacb
praakhya <67508200+praakhya@users.noreply.github.com>
2023-08-20 00:59:28 +0530

    sll and dll

---
+++ Java/DoublyLinkedList/Node.java
@@ -0,0 +1,13 @@
+package DoublyLinkedList;
+
+public class Node {
+    int value;
+    Node prev;
+    Node next;
+    public Node(int val) {
+        value = val;
+        prev = null;
+        next = null;
+    }
+    
+}
---
+++ Java/SinglyLinkedList/Node.java
@@ -0,0 +1,10 @@
+package SinglyLinkedList;
+
+public class Node {
+    int value;
+    Node next;
+    public Node(int val) {
+        value = val;
+        next= null;
+    }
+}
