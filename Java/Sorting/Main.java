2e6026020ab5a97131da9390952a480a39957735
praakhya <67508200+praakhya@users.noreply.github.com>
2023-08-25 10:07:48 +0530

    Insertion sort java

--- Java/Sorting/Main.java
+++ Java/Sorting/Main.java
@@ -1,6 +1,13 @@
-package Sorting;
+package sorting;
 import java.util.Scanner;
 public class Main {
+    public static void displayArr(int[] arr) {
+        System.out.print("[ ");
+        for (int i=0; i<arr.length; ++i) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println("]");
+    }
     public static void main(String args[]) {
         int n=10;
         int[] a = new int[n];
@@ -8,9 +15,10 @@ public class Main {
         for (int i=0; i<n; ++i) {
             a[i] = Integer.parseInt(obj.next());
         }
-        System.out.println(a);
-        InsertionSort(a);
+        InsertionSort sorter = new InsertionSort();
+        displayArr(a);
+        sorter.sort(a);
+        displayArr(a);
         obj.close();
-
     }
 }
